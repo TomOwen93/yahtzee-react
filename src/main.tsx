629857eb3f63a050b302e1382ed0e741a1fa5729
TomOwen93 <mr.tsowen@gmail.com>
2023-09-11 18:07:42 +0100

    add dark mode toggle

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <ChakraProvider>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
     </ChakraProvider>
 );
